10d7dfeaf3c29b30d575fc33da6380dabeaf0c96
LAPTOP-M94U4P14\black <black66685@gmail.com>
2023-01-30 19:42:15 -0300

    mi primer commit

---
+++ funciones.js
@@ -0,0 +1,19 @@
+document.getElementById('h2');
+function mOver(obj) {
+  obj.innerHTML = "Fernández Ángel David Abel"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Desarrollador web"
+}
+
+document.getElementById('demotex');
+function mOver(obj) {
+  obj.innerHTML = "Formación Académica"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Front-End"
+}
+
+
